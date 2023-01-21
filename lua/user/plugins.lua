646407bd762f38dc5c3b5aabc132e8b60b69031e
Pawnnub <nathany094@gmail.com>
2023-01-20 21:55:04 -0800

    Migrated to Lazy.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,104 +1,83 @@
-local fn = vim.fn
-
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing Packer, please close and reopen Neovim.")
-	vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "plugins.lua",
-	command = "source <afile> | PackerSync",
+	command = "source <afile> | Lazy sync",
 	group = vim.api.nvim_create_augroup("packer_user_config", {}),
 })
 
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+require("lazy").setup({
+ 	"github/copilot.vim",
+ 	"wbthomason/packer.nvim",
+ 	"nvim-lua/plenary.nvim",
+ 	"nvim-lua/popup.nvim",
+ 	"tpope/vim-surround",
+ 	"tpope/vim-repeat",
+ 	"tpope/vim-fugitive",
+ 	"windwp/nvim-autopairs",
+ 	"windwp/nvim-ts-autotag",
+ 	"hrsh7th/nvim-cmp",
+ 	"hrsh7th/cmp-buffer",
+ 	"hrsh7th/cmp-path",
+ 	"hrsh7th/cmp-cmdline",
+ 	"hrsh7th/cmp-nvim-lsp",
+ 	"hrsh7th/cmp-nvim-lua",
+ 	"L3MON4D3/LuaSnip",
+ 	"saadparwaiz1/cmp_luasnip",
+ 	"rafamadriz/friendly-snippets",
+ 	{
+ 		"nvim-treesitter/nvim-treesitter",
+ 		commit = "e53950f646b0c11624280ee7c4eff97a9e0904f1",
+ 		build = function()
+ 			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+ 			ts_update()
+ 		end,
+ 	},
+ 	{
+ 		"nvim-telescope/telescope.nvim",
+ 		version = "0.1.0",
+ 	},
+ 	"nvim-treesitter/nvim-treesitter-textobjects",
+ 	"nvim-treesitter/nvim-treesitter-context",
+ 	{
+ 		"nvim-telescope/telescope-fzf-native.nvim",
+ 		build = "make",
+ 	},
+ 	"JoosepAlviste/nvim-ts-context-commentstring",
+ 	"numToStr/Comment.nvim",
+ 	"williamboman/mason.nvim",
+ 	"williamboman/mason-lspconfig.nvim",
+ 	"neovim/nvim-lspconfig",
+ 	"jose-elias-alvarez/null-ls.nvim",
+ 	"mfussenegger/nvim-dap",
+ 	"rcarriga/nvim-dap-ui",
+ 	"mxsdev/nvim-dap-vscode-js",
+ 	"RRethy/vim-illuminate",
+ 	"doums/darcula",
+ 	"mg979/vim-visual-multi",
+ 	"lewis6991/gitsigns.nvim",
+ 	"kyazdani42/nvim-web-devicons",
+ 	"kyazdani42/nvim-tree.lua",
+ 	"akinsho/bufferline.nvim",
+ 	"akinsho/nvim-toggleterm.lua",
+ 	"nvim-lualine/lualine.nvim",
+ 	"moll/vim-bbye",
+ 	"ahmedkhalf/project.nvim",
+ 	"jose-elias-alvarez/typescript.nvim",
+ 	"lukas-reineke/indent-blankline.nvim",
+ 	{
+ 		"nvim-neotest/neotest",
+ 		dependencies = { "haydenmeade/neotest-jest", "olimorris/neotest-rspec", "marilari88/neotest-vitest" },
+ 	}
 })
-
-return packer.startup(function(use)
-	use("github/copilot.vim")
-	use("wbthomason/packer.nvim")
-	use("nvim-lua/plenary.nvim")
-	use("nvim-lua/popup.nvim")
-	use("tpope/vim-surround")
-	use("tpope/vim-repeat")
-	use("tpope/vim-fugitive")
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
-	use("rafamadriz/friendly-snippets")
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		commit = "e53950f646b0c11624280ee7c4eff97a9e0904f1",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	})
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-	})
-	use({ "nvim-treesitter/nvim-treesitter-textobjects", requires = { "nvim-treesitter/nvim-treesitter" } })
-	use({ "nvim-treesitter/nvim-treesitter-context", requires = { "nvim-treesitter/nvim-treesitter" } })
-	use({
-		"nvim-telescope/telescope-fzf-native.nvim",
-		run = "make",
-	})
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-	use("numToStr/Comment.nvim")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("mfussenegger/nvim-dap")
-	use("rcarriga/nvim-dap-ui")
-	use("mxsdev/nvim-dap-vscode-js")
-	use("RRethy/vim-illuminate")
-	use("doums/darcula")
-	use("mg979/vim-visual-multi")
-	use("lewis6991/gitsigns.nvim")
-	use("kyazdani42/nvim-web-devicons")
-	use("kyazdani42/nvim-tree.lua")
-	use("akinsho/bufferline.nvim")
-	use("akinsho/nvim-toggleterm.lua")
-	use("nvim-lualine/lualine.nvim")
-	use("moll/vim-bbye")
-	use("ahmedkhalf/project.nvim")
-	use("lewis6991/impatient.nvim")
-	use("jose-elias-alvarez/typescript.nvim")
-	use("lukas-reineke/indent-blankline.nvim")
-	use({
-		"nvim-neotest/neotest",
-		requires = { "haydenmeade/neotest-jest", "olimorris/neotest-rspec", "marilari88/neotest-vitest" },
-	})
-
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
