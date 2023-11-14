c9253970b0463f7b16317882d4fa4bbeb375a4cf
Pawnnub <nathany094@gmail.com>
2023-11-13 20:27:43 -0800

    Updating things

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,12 +1,12 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
---[[ local diagnostics = null_ls.builtins.diagnostics ]]
+local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	sources = {
-		--[[ diagnostics.eslint_d, ]]
+		diagnostics.eslint_d,
 		require("typescript.extensions.null-ls.code-actions"),
 		code_actions.gitsigns,
 		formatting.stylua,
